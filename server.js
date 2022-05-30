e0882f53de0f911ca8ffca00c8018880dca8f46a
bobbykim89 <bobby.sihun.kim@gmail.com>
2022-05-29 22:19:37 -0700

    edit server.js to handle static assets directory

--- server.js
+++ server.js
@@ -1,38 +1,38 @@
 if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
+  require('dotenv').config()
 }
-const express = require('express');
-const path = require('path');
-const mongoSanitize = require('express-mongo-sanitize');
-const helmet = require('helmet');
-const connectDB = require('./config/connectDB');
-const app = express();
+const express = require('express')
+const path = require('path')
+const mongoSanitize = require('express-mongo-sanitize')
+const helmet = require('helmet')
+const connectDB = require('./config/connectDB')
+const app = express()
 
 // Connect Database
-connectDB();
+connectDB()
 
 // Init Middleware
-app.use(express.json({ extended: false }));
-app.use(mongoSanitize());
-app.use(helmet());
+app.use(express.json({ extended: false }))
+app.use(mongoSanitize())
+app.use(helmet())
 
 const scriptSrcUrls = [
   'https://cdn.jsdelivr.net',
   'https://cdnjs.cloudflare.com',
   'https://cdn.jsdelivr.net',
-];
+]
 const styleSrcUrls = [
   'https://cdn.jsdelivr.net',
   'https://cdnjs.cloudflare.com',
   'https://fonts.googleapis.com',
-];
-const connectSrcUrls = ['https://api.openweathermap.org'];
-const defaultSrcUrls = ['https://mangoplanner.herokuapp.com/'];
+]
+const connectSrcUrls = ['https://api.openweathermap.org']
+const defaultSrcUrls = ['https://mangoplanner.herokuapp.com/']
 const fontSrcUrls = [
   'https://cdnjs.cloudflare.com',
   'https://fonts.googleapis.com',
   'https://fonts.gstatic.com',
-];
+]
 app.use(
   helmet.contentSecurityPolicy({
     directives: {
@@ -53,21 +53,21 @@ app.use(
       fontSrc: ["'self'", ...fontSrcUrls],
     },
   })
-);
+)
 
 // Define Routes
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/plans', require('./routes/plans'));
+app.use('/api/users', require('./routes/users'))
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/plans', require('./routes/plans'))
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static('client/dist'))
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+  })
 }
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
